554d55729cdf401ff3cc9a2bfa2ddd9ce4765e86
Lukas Plattner <lukas01@gmail.com>
2014-05-22 17:41:31 +0200

    overwrite with olindata version

---
+++ manifests/fileserver.pp
@@ -0,0 +1,14 @@
+class puppet::fileserver {
+
+  ::concat { '/etc/puppet/fileserver.conf':
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+  }
+
+  ::concat::fragment { 'puppet_fileserver_conf_header':
+    target  => '/etc/puppet/fileserver.conf',
+    order   => 00,
+    source => 'puppet:///modules/puppet/fileserver-header.conf',
+  }
+}
